950a832b90c3211af74fabd128301b3d7380521a
Douglas Mendes <mendes-barreto@live.com>
2016-01-06 18:00:41 -0200

    Synx folders

---
+++ HypnoDouglas/HypnoDouglas/Classes/DMBHypnosisViewController.h
@@ -0,0 +1,13 @@
+//
+//  DMBHypnosisViewController.h
+//  Hypnosister
+//
+//  Created by Douglas Barreto on 1/6/16.
+//  Copyright © 2016 Douglas Mendes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMBHypnosisViewController : UIViewController
+
+@end
