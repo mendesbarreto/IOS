368f6e472f73eb2492df55afb24afac5e09d79aa
Douglas Mendes <mendes-barreto@live.com>
2016-01-06 17:57:38 -0200

    Novo projeto psico

---
+++ HypnoDouglas/HypnoDouglas/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HypnoDouglas
+//
+//  Created by Douglas Barreto on 1/6/16.
+//  Copyright © 2016 Douglas Mendes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ HypnoDouglas/HypnoDouglasTests/HypnoDouglasTests.m
@@ -0,0 +1,39 @@
+//
+//  HypnoDouglasTests.m
+//  HypnoDouglasTests
+//
+//  Created by Douglas Barreto on 1/6/16.
+//  Copyright © 2016 Douglas Mendes. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HypnoDouglasTests : XCTestCase
+
+@end
+
+@implementation HypnoDouglasTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
