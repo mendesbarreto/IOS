526dbe071b4aea2216f9a911428ac7ed10d78315
Douglas Mendes <mendes-barreto@live.com>
2016-01-21 17:34:16 -0200

    New JsonSerealization project

--- Bike 2 Work/Pods/Target Support Files/Pods-Bike 2 Work/Pods-Bike 2 Work-frameworks.sh
+++ Bike 2 Work/Pods/Target Support Files/Pods-Bike 2 Work/Pods-Bike 2 Work-frameworks.sh
@@ -84,8 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "Pods-Bike 2 Work/SwiftyJSON.framework"
+  install_framework "Pods-Bike 2 Work/Alamofire.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "Pods-Bike 2 Work/SwiftyJSON.framework"
+  install_framework "Pods-Bike 2 Work/Alamofire.framework"
 fi
