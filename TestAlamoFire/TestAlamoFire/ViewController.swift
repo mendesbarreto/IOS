21a0a731995f66596fccf0ec4b13bfdbeeb31dd1
Douglas Mendes <mendes-barreto@live.com>
2016-02-01 18:37:17 -0200

    Testando Alamo fire

--- TestAlamoFire/TestAlamoFire/ViewController.swift
+++ TestAlamoFire/TestAlamoFire/ViewController.swift
@@ -1,25 +1,25 @@
 //
 //  ViewController.swift
-//  GetAnUmbrella
+//  TestAlamoFire
 //
-//  Created by Douglas Barreto on 1/29/16.
+//  Created by Douglas Barreto on 2/1/16.
 //  Copyright © 2016 Concrete Solutions. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
+
+
 }
 
